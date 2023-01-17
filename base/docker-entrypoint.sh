b647290658e4032e467dd2d56f4d750d72670961
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-01-17 16:04:05 +0100

    fix bad paths in entrypoint

--- base/docker-entrypoint.sh
+++ base/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[ -f /usr/local/root/install/bin/thisroot.sh ] && source /usr/local/root/install/bin/thisroot.sh
-[ -f /usr/local/garfieldpp/install/share/Garfield/setupGarfield.sh ] && source /usr/local/garfieldpp/install/share/Garfield/setupGarfield.sh
-[ -f /usr/local/geant4/install/bin/geant4.sh ] && source /usr/local/geant4/install/bin/geant4.sh
+[ -f /usr/local/root/bin/thisroot.sh ] && source /usr/local/root/bin/thisroot.sh
+[ -f /usr/local/garfieldpp/share/Garfield/setupGarfield.sh ] && source /usr/local/garfieldpp/share/Garfield/setupGarfield.sh
+[ -f /usr/local/geant4/bin/geant4.sh ] && source /usr/local/geant4/bin/geant4.sh
 
 exec "$@"
