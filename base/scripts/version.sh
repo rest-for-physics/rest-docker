ae46a0de58774cdaaf638e6fa7a558d696afcb33
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-01-19 09:51:39 +0100

    supressed some output from version script

--- base/scripts/version.sh
+++ base/scripts/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-command -v root-config && echo "- ROOT_VERSION: $(root-config --version)" || echo "- ROOT not found!"
-command -v geant4-config && echo "- GEANT4_VERSION: $(geant4-config --version)" || echo "- GEANT4 not found!"
+command -v root-config &> /dev/null && echo "- ROOT_VERSION: $(root-config --version)" || echo "- ROOT not found!"
+command -v geant4-config &> /dev/null && echo "- GEANT4_VERSION: $(geant4-config --version)" || echo "- GEANT4 not found!"
 test -v GARFIELD_VERSION && echo "- GARFIELD_VERSION: $GARFIELD_VERSION" || echo "- GARFIELD not found!"
